c5bb7325aa9c50e42b072f57cc594cea76ed0a97
netng <net.nandang@gmail.com>
2022-05-10 10:15:39 +0700

    updating main func

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/netng/app-say-hello
+
+go 1.18
+
+require github.com/netng/go-say-hello/v2 v2.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	sayhello "github.com/netng/go-say-hello/v2"
+)
+
+func main() {
+
+	fmt.Println(sayhello.SayHello("nandang"))
+
+}
